531add931039042e84edf3dda03787a6295583b1
Chetan <itschetan.2095@gmail.com>
2023-01-13 23:36:36 +0530

    First Commit

---
+++ Larger_Smaller.py
@@ -0,0 +1,9 @@
+# Input two number and display larger/smaller number:
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+if num1 > num2:
+    print(num1, ">", num2)
+elif num1 == num2:
+    print(num1, "=", num2)
+else:
+    print(num1, "<", num2)
