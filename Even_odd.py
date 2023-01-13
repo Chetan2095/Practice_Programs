9f9ba3646b30da04caa3f9faee2dea56711d926c
Chetan <itschetan.2095@gmail.com>
2023-01-13 23:44:39 +0530

    First Commit

---
+++ Even_odd.py
@@ -0,0 +1,7 @@
+# Write a python program to check whether entered number od Even or Odd:-
+print("Enter a number to check Even/Odd: ", end=" ")
+n = int(input())
+if n % 2 ==0:
+    print(n, "is Even.")
+else:
+    print(n, "is ODD.")
